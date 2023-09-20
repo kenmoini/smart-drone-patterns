27b6d56fa2b350b0c9a2f4f8fd881eed6c53fe0f
Ken Moini <ken@kenmoini.com>
2023-09-20 15:48:59 -0400

    stream app bump

--- apps/drone-control/api.py
+++ apps/drone-control/api.py
@@ -1,27 +1,29 @@
-from djitellopy import Tello
+#from djitellopy import Tello
+import tello
 from threading import Thread
 import time, cv2
 
 epoch_time = str(time.time())
 keepRecording = True
-tello = Tello()
+drone = tello.Tello()
 
 print("Connecting to Tello...")
-tello.end()
-tello.connect()
 
-print("Battery: " + str(tello.get_battery()) + "%")
+drone.connect()
+
+print("Battery: " + str(drone.get_battery()) + "%")
 
 print("Starting video stream...")
-tello.streamon()
-frame_read = tello.get_frame_read()
+drone.streamon()
+frame_read = drone.get_frame_read()
 
 def videoRecorder():
     height, width, _ = frame_read.frame.shape
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    force = cv2.VideoWriter_fourcc(*'XVID')
     #fourcc = cv2.VideoWriter_fourcc(*"mp4v")
     #video = cv2.VideoWriter('video-'+epoch_time+'.avi', cv2.VideoWriter_fourcc(*'avc1'), 30, (width, height))
-    video = cv2.VideoWriter('video-'+epoch_time+'.avi', fourcc, 30, (width, height))
+    video = cv2.VideoWriter('video-'+epoch_time+'.avi', force, 20.0, (640, 480))
+    frame = cv2.flip(frame, 0)
     #video = cv2.VideoWriter('video-'+epoch_time+'.mp4', fourcc, 30, (width, height))
 
     while keepRecording:
@@ -31,17 +33,17 @@ def videoRecorder():
     video.release()
 
 def scanSurroundings():
-    tello.takeoff()
-    tello.rotate_counter_clockwise(45)
+    drone.takeoff()
+    drone.rotate_counter_clockwise(45)
     time.sleep(1)
-    tello.rotate_counter_clockwise(45)
+    drone.rotate_counter_clockwise(45)
     time.sleep(1)
-    tello.rotate_counter_clockwise(45)
+    drone.rotate_counter_clockwise(45)
     time.sleep(1)
-    tello.rotate_counter_clockwise(45)
+    drone.rotate_counter_clockwise(45)
     time.sleep(1)
-    tello.rotate_clockwise(180)
-    tello.land()
+    drone.rotate_clockwise(180)
+    drone.land()
 
 recorder = Thread(target=videoRecorder)
 recorder.start()
@@ -53,4 +55,4 @@ time.sleep(3)
 keepRecording = False
 recorder.join()
 
-tello.streamoff()
+drone.streamoff()

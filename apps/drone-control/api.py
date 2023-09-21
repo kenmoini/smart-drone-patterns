597f3009902f0dde5a4938152f33fc60255b0557
Ken Moini <ken@kenmoini.com>
2023-09-21 11:05:38 -0400

    stream app bump

--- apps/drone-control/api.py
+++ apps/drone-control/api.py
@@ -30,19 +30,21 @@ def videoRecorderCV():
     height, width, _ = frame_read.frame.shape
     #fourcc = cv2.VideoWriter_fourcc(*'XVID')
     fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-    video = cv2.VideoWriter('video-'+epoch_time+'.mp4', fourcc, 15, (width, height))
+    video = cv2.VideoWriter('video-'+epoch_time+'.mp4', fourcc, 30, (width, height))
 
     while keepRecording:
         video.write(frame_read.frame)
-        time.sleep(1 / 15)
+        time.sleep(1 / 30)
 
     video.release()
 
 def scanSurroundings():
     try:
         print("Running flight pattern")
+        print("Taking off...")
         drone.takeoff()
-        time.sleep(5)
+        print("Sleeping for 3 seconds...")
+        time.sleep(3)
         #drone.rotate_counter_clockwise(45)
         #time.sleep(1)
         #drone.rotate_counter_clockwise(45)
@@ -52,12 +54,15 @@ def scanSurroundings():
         #drone.rotate_counter_clockwise(45)
         #time.sleep(1)
         #drone.rotate_counter_clockwise(360)
-        drone.send_rc_control(0, 0, 0, 60)
-        time.sleep(10)
+        print("Rotating...")
+        drone.send_rc_control(0, 0, 0, 30)
+        print("Sleeping for 12 seconds...")
+        time.sleep(12)
+        print("Landing...")
         drone.land()
     except:
         print("Hit exception in flight pattern execution!")
-        drone.land()
+        #drone.land()
 
 print("Starting recording...")
 recorder = Thread(target=videoRecorderCV)

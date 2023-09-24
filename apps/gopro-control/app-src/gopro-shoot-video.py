d333858475d602f441fef764003508223c25fb6a
Ken Moini <ken@kenmoini.com>
2023-09-23 23:20:37 -0400

    gopro bump

--- apps/gopro-control/app-src/gopro-shoot-video.py
+++ apps/gopro-control/app-src/gopro-shoot-video.py
@@ -1,12 +1,16 @@
 from goprocam import GoProCamera, constants
 import time
 
+videoLength = os.environ.get("VIDEO_LENGTH", "15")
+pathToSavedVideos = os.environ.get("PATH_TO_SAVED_VIDEOS", "videos")
+
 goproCamera = GoProCamera.GoPro()
 
 if goproCamera.IsRecording():
     print("Camera is already recording! Exiting...")
 else:
-    print("Recording for 10 seconds")
-    goproCamera.shoot_video(10)
+    print("Recording for " + videoLength + " seconds")
+    recordedVideo = goproCamera.shoot_video(int(videoLength))
+
     epoch_time = str(int(time.time()))
-    goproCamera.downloadLastMedia()
+    goproCamera.downloadLastMedia(recordedVideo, path=pathToSavedVideos, custom_filename="GOPRO_" + epoch_time + ".MP4")

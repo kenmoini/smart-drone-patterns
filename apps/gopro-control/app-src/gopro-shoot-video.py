41f684f090de5d1570c8246c29f3909198c034a7
Ken Moini <ken@kenmoini.com>
2023-09-23 23:16:51 -0400

    gopro bump

--- apps/gopro-control/app-src/gopro-shoot-video.py
+++ apps/gopro-control/app-src/gopro-shoot-video.py
@@ -9,4 +9,4 @@ else:
     print("Recording for 10 seconds")
     goproCamera.shoot_video(10)
     epoch_time = str(int(time.time()))
-    goproCamera.downloadLastMedia('videos/', epoch_time)
+    goproCamera.downloadLastMedia()

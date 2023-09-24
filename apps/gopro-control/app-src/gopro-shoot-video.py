c159f8f1f64baf06c20dca12ee6db5632577fa15
Ken Moini <ken@kenmoini.com>
2023-09-23 23:22:05 -0400

    gopro bump

--- apps/gopro-control/app-src/gopro-shoot-video.py
+++ apps/gopro-control/app-src/gopro-shoot-video.py
@@ -2,7 +2,6 @@ from goprocam import GoProCamera, constants
 import time, os
 
 videoLength = os.environ.get("VIDEO_LENGTH", "15")
-pathToSavedVideos = os.environ.get("PATH_TO_SAVED_VIDEOS", "videos")
 
 goproCamera = GoProCamera.GoPro()
 
@@ -13,4 +12,4 @@ else:
     recordedVideo = goproCamera.shoot_video(int(videoLength))
 
     epoch_time = str(int(time.time()))
-    goproCamera.downloadLastMedia(recordedVideo, path=pathToSavedVideos, custom_filename="GOPRO_" + epoch_time + ".MP4")
+    goproCamera.downloadLastMedia(recordedVideo, custom_filename="GOPRO_" + epoch_time + ".MP4")

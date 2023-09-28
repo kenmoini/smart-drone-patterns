a13e05b7631b7f60ea50350bb83d8ef895ab79a7
Ken Moini <ken@kenmoini.com>
2023-09-28 11:22:07 -0400

    move to s3 init

--- apps/symphony/app-src/static/js/99_common.js
+++ apps/symphony/app-src/static/js/99_common.js
@@ -3,3 +3,8 @@ function progressFail() {
     jQuery("#progressMessage strong").text("Failed!");
     jQuery("#progressBar .progress-bar").removeClass('bg-primary').addClass('bg-danger').css('width', '100%');
 }
+
+function progressMover(text, percent) {
+    jQuery("#progressMessage strong").text(text);
+    jQuery("#progressBar .progress-bar").css('width', percent + '%');
+}
--- apps/symphony/app-src/static/js/edge-delivery.js
+++ apps/symphony/app-src/static/js/edge-delivery.js
@@ -20,26 +20,36 @@ jQuery( document ).ready(function() {
     jQuery( "#startBtn" ).on( "click", function() {
         // Show the workzone
         jQuery("#workZone").removeClass('d-none');
-        jQuery("#progressMessage strong").text("Starting...");
-        jQuery("#progressBar .progress-bar").css('width', '10%');
-        jQuery("#progressMessage strong").text("Starting...");
+        progressMover("Starting...", 10);
         setTimeout(() => {
             console.log("Delayed for 0.5 second.");
         }, "500");
 
-        jQuery("#progressMessage strong").text("Initializing camera...");
-        jQuery("#progressBar .progress-bar").css('width', '15%');
+        progressMover("Initializing camera...", 15);
         
         // Call out to the gopro-control service
         // Swich upon the status, if successful then call out to the s3-shipper service
         var goproData_r = jQuery.get( configData.goproControl.endpoint, function(data) {
             goproData = JSON.parse(data);
             console.log(goproData);
-
-            alert( "success" );
         })
-        .done(function() {
-            //alert( "second success" );
+        .done(function(data) {
+            goproData = JSON.parse(data);
+            if (data.status == "success") {
+                console.log("goproData: " + goproData);
+                progressMover("Video captured!", 20);
+                setTimeout(() => {
+                    console.log("Delayed for 0.5 second.");
+                }, "500");
+                progressMover("Uploading to S3...", 30);
+                var s3Data_r = jQuery.get( configData.s3Shipper.endpoint, function(data) {
+                    s3Data = JSON.parse(data);
+                    console.log(s3Data);
+                })
+            }
+            else {
+                progressFail();
+            }
         })
         .fail(function() {
             progressFail();

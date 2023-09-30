51ef5e1f99c7dff9ce11c6b9a20ec802d9f0a42e
Ken Moini <ken@kenmoini.com>
2023-09-29 22:59:26 -0400

    tooltip

--- apps/symphony/app-src/static/js/99_common.js
+++ apps/symphony/app-src/static/js/99_common.js
@@ -19,14 +19,14 @@ function checkWifiStatus(targetAP) {
         if (wifiData.activeNetwork.length > 0) {
             if (wifiData.activeNetwork[0][2] == targetAP) {
                 console.log("Connected to " + targetAP);
-                jQuery("#wifiStatus").removeClass('red').addClass('green');
+                jQuery("#wifiStatus").removeClass('red').addClass('green').attr('title', "Connected to " + targetAP);
             } else {
                 console.log("Connected to " + wifiData.activeNetwork[0][2]);
-                jQuery("#wifiStatus").removeClass('green').addClass('orange');
+                jQuery("#wifiStatus").removeClass('green').addClass('orange').attr('title', "Connected to " + wifiData.activeNetwork[0][2]);
             }
         } else {
             console.log("Not connected to any AP!");
-            jQuery("#wifiStatus").removeClass('green').addClass('red');
+            jQuery("#wifiStatus").removeClass('green').addClass('red').attr('title', "Not connected to any AP!");
         }
     })
     .fail(function() {

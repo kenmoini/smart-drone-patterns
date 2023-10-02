679245c4bb3d346935e8060932ca25233ab17c72
Ken Moini <ken@kenmoini.com>
2023-10-01 22:03:17 -0400

    add py-server app

---
+++ apps/py-server/app-src/main.py
@@ -0,0 +1,27 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+import ssl, os
+
+HOST = os.environ.get("PY_HTTP_SERVER_HOST", "0.0.0.0")
+PORT = os.environ.get("PY_HTTP_SERVER_PORT", 7999)
+DIRECTORY = os.environ.get("PY_HTTP_SERVER_DIRECTORY", "./")
+tlsCert = os.environ.get("PY_HTTP_SERVER_TLS_CERT", "")
+tlsKey = os.environ.get("PY_HTTP_SERVER_TLS_KEY", "")
+
+
+if __name__ == "__main__":
+    try:
+        httpd = HTTPServer((HOST, PORT), SimpleHTTPRequestHandler)
+
+        if tlsCert != "" and tlsKey != "":
+            print("Starting Simple HTTP Server on port " + str(PORT) + " and host " + str(HOST) + " with TLS cert " + str(tlsCert) + " and TLS key " + str(tlsKey))
+            httpd.socket = ssl.wrap_socket(httpd.socket, 
+                    keyfile=tlsKey, certfile=tlsCert, server_side=True)
+
+        else:
+            print("Starting Simple HTTP Server on port " + str(PORT) + " and host " + str(HOST))
+
+        httpd.serve_forever()
+
+    except KeyboardInterrupt:
+        pass
+        httpd.server_close()
---
+++ apps/py-server/app-src/start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Starting py-server"
+
+python3 main.py

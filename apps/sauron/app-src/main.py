a9c2df9e98d69c1b355b88b658b13545a882d086
Ken Moini <ken@kenmoini.com>
2023-10-11 10:19:07 -0400

    thanks i hate it

--- apps/sauron/app-src/main.py
+++ apps/sauron/app-src/main.py
@@ -9,6 +9,9 @@ tlsKey = os.environ.get("SAURON_HTTP_SERVER_TLS_KEY", "")
 kafkaTopic = os.environ.get("KAFKA_TOPIC", "gopro-videos")
 kafkaEndpoint = os.environ.get("KAFKA_ENDPOINT", "my-cluster-source-kafka-bootstrap.kafka-cluster.svc.cluster.local:9092")
 
+print("Connecting to: " + kafkaEndpoint)
+print("Observing topic: " + kafkaTopic)
+
 consumer = kafka.KafkaConsumer(kafkaTopic, bootstrap_servers=[kafkaEndpoint])
 
 for message in consumer:

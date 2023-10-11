2e184aa6c503fd0e2ac46eeae150d496a5036b87
Ken Moini <ken@kenmoini.com>
2023-10-11 13:29:09 -0400

    ffffffffff

--- apps/sauron/app-src/main.py
+++ apps/sauron/app-src/main.py
@@ -1,22 +1,43 @@
 import os, json
 import kafka
 import urllib.request
+import boto3
 
+####################
+## Setup Flask Variables
 HOST = os.environ.get("SAURON_HTTP_SERVER_HOST", "0.0.0.0")
 PORT = os.environ.get("SAURON_HTTP_SERVER_PORT", 6669)
 tlsCert = os.environ.get("SAURON_HTTP_SERVER_TLS_CERT", "")
 tlsKey = os.environ.get("SAURON_HTTP_SERVER_TLS_KEY", "")
 
+####################
+# Setup S3 client
+s3SecretPath = os.environ.get("S3_SECRET_PATH", "/var/run/secrets/s3/")
+s3EndpointLink = os.environ.get("S3_ENDPOINT_LINK", "s3.us-east-2.amazonaws.com")
+verifySSLEnv = os.environ.get("S3_VERIFY_SSL", "False")
+
+verifySSL = False
+if verifySSLEnv.lower() == "true":
+    verifySSL = True
+
+access_key_id_path = open(s3SecretPath + 'access_key_id', "r")
+access_key_secret_path = open(s3SecretPath + 'access_key_secret', "r")
+access_key_id = access_key_id_path.read().strip()
+access_key_secret = access_key_secret_path.read().strip()
+
+####################
+# Setup Kafka client
 kafkaTopic = os.environ.get("KAFKA_TOPIC", "gopro-videos")
 kafkaEndpoint = os.environ.get("KAFKA_ENDPOINT", "my-cluster-kafka-bootstrap.kafka-cluster.svc.cluster.local:9092")
 
 print("Connecting to: " + kafkaEndpoint)
 print("Observing topic: " + kafkaTopic)
 
-#consumer = kafka.KafkaConsumer(kafkaTopic, bootstrap_servers=[kafkaEndpoint], value_deserializer=lambda m: json.loads(m.decode('ascii')))
 consumer = kafka.KafkaConsumer(kafkaTopic, bootstrap_servers=[kafkaEndpoint])
 
+####################
 for message in consumer:
+    print("===== Received message!")
     print(message)
     # message value and key are raw bytes -- decode if necessary!
     # e.g., for unicode: `message.value.decode('utf-8')`
@@ -37,13 +58,13 @@ for message in consumer:
     s3Endpoint = record['responseElements']["x-minio-origin-endpoint"]
     downloadURI = s3Endpoint + "/" + bucket + "/" + fileName
 
-    print("S3 Endpoint: " + s3Endpoint)
-    print("Bucket: " + bucket)
-    print("Filename: " + fileName)
-    print("URI: " + downloadURI)
-    print("Event Type: " + eventType)
+    print("- S3 Endpoint: " + s3Endpoint)
+    print("- Bucket: " + bucket)
+    print("- Filename: " + fileName)
+    print("- URI: " + downloadURI)
+    print("- Event Type: " + eventType)
 
-    print ("%s %s:%d:%d: key=%s value=%s" % (eventType, topic, partition, offset, key, value))
+    #print ("%s %s:%d:%d: key=%s value=%s" % (eventType, topic, partition, offset, key, decodedValue))
 
     # Next, download the file from S3
     urllib.request.urlretrieve(downloadURI, "inf_" + fileName)

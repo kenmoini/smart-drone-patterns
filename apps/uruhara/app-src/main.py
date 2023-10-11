10f4151239d32fb10f881f0dc44aa7206b44b11d
Ken Moini <ken@kenmoini.com>
2023-10-11 18:02:45 -0400

    add more process workflow

--- apps/uruhara/app-src/main.py
+++ apps/uruhara/app-src/main.py
@@ -1,6 +1,6 @@
-# 
+# Uruhara runs the model inference and then parses the output. It then returns the parsed output in JSON format.
 from pprint import pprint
-import pathlib
+import pathlib, json
 import os
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
@@ -34,18 +34,53 @@ def upload():
 
         data = request.get_json()
         print(data)
+
+        # Determine predicted file name
+        predictedFileName = data['fileName'].replace("inf_", "pred_")
         # expects {"fileType": "video", "fileName": "/shared-data/GOPRO_VID069420.mp4"}
         if data['fileType'] == "video":
             print("Processing video file: " + data['fileName'])
+            status['fileType'] = 'video'
 
-            rc = os.system("darknet version")
+            predictionDataFileName = os.path.splitext(data['fileName'].replace("inf_", "pred_"))[0] + ".txt"
+            predictionJSONFileName = os.path.splitext(data['fileName'].replace("inf_", "pred_"))[0] + ".json"
+
+            rc = os.system("darknet detector demo -dont_show ./models/hats/hats.data ./models/hats/hats.cfg ./models/hats/hats_best.weights " + data['fileName'] + " -out_filename " + predictedFileName + " -ext_output > " + predictionDataFileName)
             if rc == 0:
                 status['darknet'] = 'ok'
+                status['predictedFileName'] = predictedFileName
+                # Next run the parser
+                prc = os.system("python3 parse_output.py -i " + predictionDataFileName + " -o " + predictionJSONFileName)
+                if prc == 0:
+                    status['outputParser'] = 'ok'
+                    status['predictionJSONFileName'] = predictionJSONFileName
+                    status['predictionData'] = json.load(open(predictionJSONFileName, "r"))
+                else:
+                    status['outputParser'] = 'failed'
             else:
                 status['darknet'] = 'failed'
 
         elif data['fileType'] == "image":
             print("Processing image file: " + data['fileName'])
+            status['fileType'] = 'image'
+            colorSwappedFilename = os.path.splitext(predictedFileName)[0] + "-swapped." + os.path.splitext(predictedFileName)[1]
+
+            rc = os.system("darknet detector test -dont_show ./models/hats/hats.data ./models/hats/hats.cfg ./models/hats/hats_best.weights " + data['fileName'] + " -out hat_prediction.json")
+            if rc == 0:
+                status['darknet'] = 'ok'
+                # Copy the prediction.jpg file to where the predicted file name is
+                os.system("cp predictions.jpg " + predictedFileName)
+                status['predictedFileName'] = predictedFileName
+
+                # Next run the script to swap the colorspace
+                scrc = os.system("python3 swap-colorspace.py -i " + predictedFileName + " -o " + colorSwappedFilename)
+                if scrc == 0:
+                    status['swapColorspace'] = 'ok'
+                    status['swappedFileName'] = colorSwappedFilename
+                else:
+                    status['swapColorspace'] = 'failed'
+            else:
+                status['darknet'] = 'failed'
         
         return status
 
---
+++ apps/uruhara/app-src/parse_output.py
@@ -0,0 +1,107 @@
+# Description: This script parses the output of the video inference text blob into JSON
+# python3 parse_output.py -i <input file> -o <output file>
+# python3 parse_output.py -i output-test.txt -o output-test.json
+import json, argparse, os, sys
+
+# Define and parse the input arguments
+argParser = argparse.ArgumentParser()
+argParser.add_argument("-i", "--input", help="input text stream")
+argParser.add_argument("-o", "--output", help="output JSON file")
+args = argParser.parse_args()
+
+# Check to see if the input image exists
+if not os.path.isfile(args.input):
+    print("The input file does not exist")
+    sys.exit(1)
+
+inputTextFile = open(args.input, "r")
+
+lines = inputTextFile.readlines()
+
+video_data = {}
+frames = []
+frame = {}
+frameCount = 1
+fileNameDetected = False
+fileInfoDetected = False
+frameBlockFound = False
+
+for line in lines:
+
+    # Detect video info
+    if fileNameDetected == False:
+        if "video file:" in line:
+            video_file = line.split(" ")[2].strip()
+            video_data["video_file"] = video_file
+            fileNameDetected = True
+            continue
+    if fileInfoDetected == False:
+        if "Video stream:" in line:
+            video_width = line.split(" ")[2]
+            video_height = line.split(" ")[4]
+            video_data["video_size"] = video_width + "x" + video_height
+            fileInfoDetected = True
+            continue
+
+    # Detect when frame processing starts
+    if "Objects:" in line:
+        frameBlockFound = True
+        frame["frame_number"] = frameCount
+        frame["objects"] = []
+        continue
+
+    # Detect when frame processing ends
+    if "FPS:" in line:
+        frameBlockFound = False
+        frames.append(frame)
+        frame = {}
+        # Increase the count
+        frameCount += 1
+        continue
+
+    # Detect when stream processing ends
+    if "input video stream closed" in line:
+        video_data["frame_count"] = frameCount
+        break
+
+    # Parse frame data
+    if frameBlockFound:
+        # If there is a detected object with an accuracy level
+        if "%" in line:
+            object = {}
+            classOb = line.split(":")[0].strip()
+            if classOb != "":
+                object["class"] = classOb
+                object["confidence"] = line.split(":")[1].strip().split("%")[0] + "%"
+                
+                coord = line.split("(")[1].strip().split(")")[0].split()
+                coordinates = {}
+                # Loop through the coordinate object
+                for i in range(len(coord)):
+                    c = coord[i].replace(":","").strip()
+                    if c == "left_x":
+                        coordinates["left_x"] = coord[i+1]
+                    elif c == "top_y":
+                        coordinates["top_y"] = coord[i+1]
+                    elif c == "width":
+                        coordinates["width"] = coord[i+1]
+                    elif c == "height":
+                        coordinates["height"] = coord[i+1]
+                object["coordinates"] = coordinates
+                frame["objects"].append(object)
+
+# Add frames to video data
+video_data["frames"] = frames
+
+# Close the input file
+inputTextFile.close()
+
+# Determine the output file name
+if args.output is None:
+    output_file = ".".join(video_data['video_file'].split(".")[:-1]) + "_output.json"
+else:
+    output_file = args.output
+
+# Write the output to a file
+with open(output_file, 'w') as outfile:
+    json.dump(video_data, outfile)

c8b226c4957d464d70b3c2b797eedef8abcd6908
Ken Moini <ken@kenmoini.com>
2023-10-11 11:17:17 -0400

    better container layer caching and moar record processing

--- apps/sauron/app-src/main.py
+++ apps/sauron/app-src/main.py
@@ -16,7 +16,25 @@ consumer = kafka.KafkaConsumer(kafkaTopic, bootstrap_servers=[kafkaEndpoint])
 
 for message in consumer:
     print(message)
-    print("decoded from utf-8:")
     # message value and key are raw bytes -- decode if necessary!
     # e.g., for unicode: `message.value.decode('utf-8')`
-    print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition, message.offset, message.key, message.value))
+    topic = message.topic
+    partition = message.partition
+    offset = message.offset
+    key = message.key # key is also what we use to determine the uploaded bucket/filename
+    value = message.value # value is all the data
+
+    eventType = value.EventName;
+
+    record = value.Records[0]
+    bucket = record.s3.bucket.name
+    fileName = record.s3.object.key
+    s3Endpoint = record.responseElements["x-minio-origin-endpoint"]
+
+    print("S3 Endpoint: " + s3Endpoint)
+    print("Bucket: " + bucket)
+    print("Filename: " + fileName)
+    print("URI: " + s3Endpoint + "/" + bucket + "/" + fileName)
+    print("Event Type: " + eventType)
+
+    print ("%s %s:%d:%d: key=%s value=%s" % (eventType, topic, partition, offset, key, value))

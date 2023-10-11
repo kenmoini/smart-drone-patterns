e07d5f722439a3b8519e18bf4f39847d659f4802
Ken Moini <ken@kenmoini.com>
2023-10-11 10:10:23 -0400

    bump sauron src

--- apps/sauron/app-src/main.py
+++ apps/sauron/app-src/main.py
@@ -9,7 +9,11 @@ tlsKey = os.environ.get("SAURON_HTTP_SERVER_TLS_KEY", "")
 kafkaTopic = os.environ.get("KAFKA_TOPIC", "gopro-videos")
 kafkaEndpoint = os.environ.get("KAFKA_ENDPOINT", "my-cluster-source-kafka-bootstrap.kafka-cluster.svc.cluster.local:9092")
 
-consumer = kafka.KafkaConsumer(bookstrap_servers=kafkaEndpoint, topics=kafkaTopic)
+consumer = kafka.KafkaConsumer(kafkaTopic, bootstrap_servers=[kafkaEndpoint])
 
-for msg in consumer:
-    print (msg)
+for message in consumer:
+    print(message)
+    print("decoded from utf-8:")
+    # message value and key are raw bytes -- decode if necessary!
+    # e.g., for unicode: `message.value.decode('utf-8')`
+    print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition, message.offset, message.key, message.value))

a4a83b47d0d78e07e11561a0718dcfd5f32b9568
Ken Moini <ken@kenmoini.com>
2023-10-14 15:58:21 -0400

    update video json parser

--- apps/darknet/bootstrap-apps/parse-video-inference/parse_output.py
+++ apps/darknet/bootstrap-apps/parse-video-inference/parse_output.py
@@ -1,7 +1,7 @@
 # Description: This script parses the output of the video inference text blob into JSON
 # python3 parse_output.py -i <input file> -o <output file>
 # python3 parse_output.py -i output-test.txt -o output-test.json
-import json, argparse, os, sys
+import json, argparse, os, sys, re
 
 # Define and parse the input arguments
 argParser = argparse.ArgumentParser()
@@ -68,29 +68,81 @@ for line in lines:
     if frameBlockFound:
         # If there is a detected object with an accuracy level
         if "%" in line:
-            object = {}
+            #object = {}
             # Occasionally there are multiple class objects on a single line delimited by a comma if it thinks it could be one of multiple objects
+            # The classes and the bounding box definition is separated by a tab
+            # Then the classes are separated by a comma
+            # The class and confidence are separated by a colon
 
-            classOb = line.split(":")[0].strip()
-            if classOb != "":
-                object["class"] = classOb
-                object["confidence"] = line.split(":")[1].strip().split("%")[0] + "%"
-                
-                coord = line.split("(")[1].strip().split(")")[0].split()
-                coordinates = {}
-                # Loop through the coordinate object
-                for i in range(len(coord)):
-                    c = coord[i].replace(":","").strip()
-                    if c == "left_x":
-                        coordinates["left_x"] = coord[i+1]
-                    elif c == "top_y":
-                        coordinates["top_y"] = coord[i+1]
-                    elif c == "width":
-                        coordinates["width"] = coord[i+1]
-                    elif c == "height":
-                        coordinates["height"] = coord[i+1]
-                object["coordinates"] = coordinates
-                frame["objects"].append(object)
+            # First separate the class objects and the bounding box definition
+            #classOb = line.split(":")[0].strip()
+            strippedLine = line.strip()
+            #print(strippedLine)
+
+            classOb = re.split(r'\t+', strippedLine)
+            classes = classOb[0].split(",")
+            
+            # Strip any whitespace from the class names
+            for i in range(len(classes)):
+                classes[i] = classes[i].strip()
+
+            #print(classes)
+            boundingBox = classOb[1].strip().rstrip(')').lstrip('(')
+            boundingBoxCoords = re.split(r'\s+', boundingBox)
+            #print(boundingBoxCoords)
+
+            # Process the bounding box coordinates
+            coordinates = {}
+            # Loop through the coordinate object and pair the key and value
+            for i in range(len(boundingBoxCoords)):
+                c = boundingBoxCoords[i].rstrip(':')
+                if c == "left_x":
+                    coordinates["left_x"] = boundingBoxCoords[i+1]
+                elif c == "top_y":
+                    coordinates["top_y"] = boundingBoxCoords[i+1]
+                elif c == "width":
+                    coordinates["width"] = boundingBoxCoords[i+1]
+                elif c == "height":
+                    coordinates["height"] = boundingBoxCoords[i+1]
+
+            #object["coordinates"] = coordinates
+
+            # Loop through the classes and add them to the object
+            for i in range(len(classes)):
+                individualObject = {}
+                # Split the class and confidence
+                classConfidence = classes[i].split(": ")
+                individualObject["class"] = classConfidence[0]
+                individualObject["confidence"] = classConfidence[1]
+                individualObject["coordinates"] = coordinates
+
+                # Add the individual object to the object
+                #object["objects"] = individualObject
+
+                # Add the object to the frame
+                frame["objects"].append(individualObject)
+
+
+            # This is good stuff, just needs to be reformatted for multiple objects
+            #if classOb != "":
+            #    object["class"] = classOb
+            #    object["confidence"] = line.split(":")[1].strip().split("%")[0] + "%"
+            #    
+            #    coord = line.split("(")[1].strip().split(")")[0].split()
+            #    coordinates = {}
+            #    # Loop through the coordinate object
+            #    for i in range(len(coord)):
+            #        c = coord[i].replace(":","").strip()
+            #        if c == "left_x":
+            #            coordinates["left_x"] = coord[i+1]
+            #        elif c == "top_y":
+            #            coordinates["top_y"] = coord[i+1]
+            #        elif c == "width":
+            #            coordinates["width"] = coord[i+1]
+            #        elif c == "height":
+            #            coordinates["height"] = coord[i+1]
+            #    object["coordinates"] = coordinates
+            #    frame["objects"].append(object)
 
 # Add frames to video data
 video_data["frames"] = frames
